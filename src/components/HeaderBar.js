56941154cfeccf0fa9e48e6d3be1883ad1877d29
Gihan-Madhusankha <gihanmadhusankha1999@gmail.com>
2024-02-27 15:42:57 +0530

    updated the HomeScreen

---
+++ src/components/HeaderBar.js
@@ -0,0 +1,36 @@
+/*eslint-disable*/
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+import GradientBGIcon from './GradientBGIcon';
+import ProfilePic from './ProfilePic';
+
+const HeaderBar = ({ title }) => {
+  return (
+    <View style={styles.HeaderContainer}>
+      <GradientBGIcon
+        name="menu"
+        color={COLORS.primaryLightGreyHex}
+        size={FONTSIZE.size_16}
+      />
+      <Text style={styles.HeaderText}>{title}</Text>
+      <ProfilePic />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  HeaderContainer: {
+    padding: SPACING.space_30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  HeaderText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+  },
+});
+
+export default HeaderBar;
